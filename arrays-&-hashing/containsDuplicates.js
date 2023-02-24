6e7b485b2bf6352a224f86800da901ced3ff9ae1
niv suroghon <nsuroghon@gmail.com>
2023-02-24 14:55:50 -0500

    contains duplicates solution 1

---
+++ arrays-&-hashing/containsDuplicates.js
@@ -0,0 +1,33 @@
+var containsDuplicate = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+// O of n squared
+// Memory of 1 
+
+
+var conatainsDuplicates2 = function(nums) {
+    const map = {}
+    console.log(nums)
+
+    for(el of nums) {
+        console.log(el)
+        if(map[el]) {
+            map[el] += 1
+        }
+        else {
+            map[el] = 1
+        }
+    }
+
+    return map;
+}
+
+console.log(conatainsDuplicates2([1,1,1,3,3,4,3,2,4,2]))
