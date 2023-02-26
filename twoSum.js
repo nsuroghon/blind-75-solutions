6b939e23ecdb532001b115a0e6d6bc6c6597df16
niv suroghon <nsuroghon@gmail.com>
2023-02-25 21:47:27 -0500

    hash map solution for two sum challenge

--- twoSum.js
+++ twoSum.js
@@ -16,7 +16,7 @@ function twoSum(nums, target) {
             }
         }
     }
-    
+
 }
 
 console.log(twoSum([2,7,11,15], 9))
@@ -24,13 +24,39 @@ console.log(twoSum([2,7,11,15], 9))
 // Space - 0(1)
 
 
-// Iterative with filter
-
-
-
 // Hash Map
+// Can pass through array only once using this method
 
-// Time - O(n) + O (n)
+// Logic:
+// Map = {empty}
+// [2, 7, 11, 15], target 9
 
+// At each element of array...
+// Does target - 2 (7) exist in array
+// no, so we store map[2] = 0 (the index)
+// next...
+// Does target - 7 (2) exist in array
+// Yes, return index of current element which is and map[2], which is 0
+
+
+// Check if matching 
+function twoSum2(nums,target) {
+    const map = {}
+    for(let i = 0; i < nums.length; i++) {
+        const matchingPair = target - nums[i]
+        if(map[matchingPair] !== undefined) {
+            return [map[matchingPair], i]
+        }
+        else {
+            map[nums[i]] = i
+        }
+        
+    }
+}
+
+console.log(twoSum2([0,4,3,0], 0))
+console.log(twoSum2([2,7,11,15], 9))
+
+// Time - O(n)
 // Space - O(n)
 
