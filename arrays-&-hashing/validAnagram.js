b73dd5f6642172ea3e7cd1647ec69b0b8efec683
niv suroghon <nsuroghon@gmail.com>
2023-02-25 19:37:42 -0500

    optimized hash map solution

--- arrays-&-hashing/validAnagram.js
+++ arrays-&-hashing/validAnagram.js
@@ -57,9 +57,59 @@ function validAnagram2(s, t) {
     return true;
 }
 
-// Time - O(n) + O(n) + O(n) + O(N)
-// Space - N
+// Time - O(n) + O(n) + O(n) + O(n)
+// Space - N + N
 
-console.log(validAnagram("rat", "car"))
+console.log(validAnagram2("rat", "car"))
 console.log(validAnagram2("cat", "taco"))
 console.log(validAnagram2("anagram", "nagaram"))
+
+// Hash Map solution optimized
+// 1 hash map instead of 2
+// Logic:
+// s = caat -> {a: 2, c: 1, t: 1}
+// t = taca -> {a: 2, c: 1, t: 1}
+
+// sCount[ch] === tCount[ch]
+// sCount[ch] - tCount[ch] === 0 -> anagaram
+
+// Explanation: 
+// if it an anagram and we subtract count of each char it will equal 0
+// So we can iterate through s, t
+// If char is in s we increment
+// If char is in t we decremenet
+// If it is an anagram every char will have a count of 0 at the end
+
+function validAnagram3(s,t) {
+    const count = {};
+    
+    // if lengths not equal can't be anagram
+    if(s.length !== t.length) return false;
+
+    const len = s.length;
+
+    for(let i = 0; i < len; i++) {
+        // at each iteration
+        // increment for count at s
+        // decrement for coutn at t
+        // initialize & count if doesn't exist
+        if(!count[s[i]]) count[s[i]] = 0
+        if(!count[t[i]]) count[t[i]] = 0
+        count[s[i]]++
+        count[t[i]]--
+    }
+
+    // every char in hash map must have a value of zero to be an anagram
+    for(char in count) {
+        if(count[char] !== 0) return false;
+    }
+
+    return true;
+}
+
+// Time - O(n) + O(n) + O(n) + O(n)
+// Space - N
+
+console.log(validAnagram3("rat", "car"))
+console.log(validAnagram3("cat", "taco"))
+console.log(validAnagram3("anagram", "nagaram"))
