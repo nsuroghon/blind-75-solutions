de38e9ab35194dc52add991e4685527df52125a3
niv suroghon <nsuroghon@gmail.com>
2023-02-25 19:13:21 -0500

    hash map solution for valid anagram problem

--- arrays-&-hashing/validAnagram.js
+++ arrays-&-hashing/validAnagram.js
@@ -15,7 +15,7 @@ console.log(validAnagram("anagram", "nagaram"))
 // Optimizing Sorting Solution
 
 function validAnagram1(s,t) {
-    
+
     if(s.length !== t.length) return false;
 
     const sSorted = s.split("").sort().join("")
@@ -26,28 +26,40 @@ function validAnagram1(s,t) {
 
 console.log(validAnagram1("cat", "taco"))
 console.log(validAnagram1("anagram", "nagaram"))
-// Hash Map Solution
 
+// Hash Map Solution
 function validAnagram2(s, t) {
+    if(s.length !== t.length) return false;
+
     const sMap = {}
     const tMap = {}
     
-    for(el of sSorted) {
+    for(el of s) {
         if(!sMap[el]) {
             sMap[el] = 1
         }
         else sMap[el] += 1
     }
 
-    for(el of tSorted) {
+    for(el of t) {
         if(!tMap[el]) {
             tMap[el] = 1
         }
         else tMap[el] += 1
     }
-    
-    console.log(sMap)
-    console.log(tMap)
 
-    return sMap === tMap
+    for(char in sMap) {
+        if(sMap[char] !== tMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+// Time - O(n) + O(n) + O(n) + O(N)
+// Space - N
+
+console.log(validAnagram("rat", "car"))
+console.log(validAnagram2("cat", "taco"))
+console.log(validAnagram2("anagram", "nagaram"))
