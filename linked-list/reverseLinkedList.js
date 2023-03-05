a71fde8d9598a4a53bdf560a2990b6ea4f1f830d
niv suroghon <nsuroghon@gmail.com>
2023-03-04 19:04:29 -0500

    reverse linked list iterative solution

---
+++ linked-list/reverseLinkedList.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+
+    while(curr) {
+        let temp = curr.next
+        curr.next = prev;
+        // moving pointers
+        prev = curr;
+        curr = temp;
+    }
+    
+    return prev;
+};
+
+// Time - O(N)
+// Space - O(1)
